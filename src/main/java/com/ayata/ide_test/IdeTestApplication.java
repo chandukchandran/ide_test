05d59159b72373c88ac2fcc5f937695252c0f43f
Aravind KC <aravind.kc@ayatacommerce.com>
2022-04-05 21:34:11 +0530

    project adding

---
+++ src/main/java/com/ayata/ide_test/IdeTestApplication.java
@@ -0,0 +1,14 @@
+package com.ayata.ide_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeTestApplication.class, args);
+		System.out.print("hi");
+	}
+
+}
---
+++ src/test/java/com/ayata/ide_test/IdeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayata.ide_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
